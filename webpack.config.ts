dd8ab4f378b7ae69efdf685f7886c7f65f5bddb2
demonsjostle <demons.jostle@gmail.com>
2025-03-06 17:30:25 +0700

    remove all

--- webpack.config.ts
+++
@@ -1,8 +0,0 @@
-// webpack.config.ts
-const environment = process.env.NODE_ENV || "development";
-
-if (environment === "production") {
-  module.exports = require("./config/webpack.prod.ts");
-} else {
-  module.exports = require("./config/webpack.dev.ts");
-}
